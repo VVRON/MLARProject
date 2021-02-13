b488eec41b1cae7bdaa7dfb7722585b5e3166c3f
DESKTOP\\VVRON <me@aaronakrong.com>
2021-02-13 05:46:25 +0000

    2.0.0
    -Added NavigateScenes.cs
    Unity: UI changes
    -Two scenes "tutorial" & "tutorial_graph"
    -New buttons GoToGraphScene, Show Data, Data Visualisation
    -Added On Click() functionality for moving between scenes
    -DatasetText object now a child of Show Data

---
+++ MLARProject/Assets/Scripts/NavigateScenes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NavigateScenes : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name != "tutorial")
+        {
+            SceneManager.LoadScene("tutorial");
+        }
+        else
+        {
+            SceneManager.LoadScene("tutorial_graph");
+        }
+    }
+    
+}
